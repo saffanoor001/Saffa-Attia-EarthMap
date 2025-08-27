a59f8589ccb0bd23ff2fde36d18778d890ede3ce
Saffa <saffanoor001@gmail.com>
2025-08-27 13:22:09 +0500

    location search

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
